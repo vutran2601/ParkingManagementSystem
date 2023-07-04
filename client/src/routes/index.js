26dcc1a9d002dbd76628a5728c1cf4770b8521e6
VuTran2601 <nguyenvutran2601@gmail.com>
2023-07-04 14:41:55 +0700

    import Frontend template

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,28 +1,11 @@
-// Import Layout
-import { DefaultLayout } from "../components/Layouts";
-// Import Route Pages
-import Dashboard from "../pages/Dashboard";
-import Login from "../pages/User/Login";
-import Register from "../pages/User/Register";
-import Profile from "../pages/User/Profile";
-import History from "../pages/History";
-import Rooms from "../pages/Rooms";
-import Devices from "../pages/Devices"
+import { useRoutes } from 'react-router-dom';
 
-// Not Required Login
-// layout:null --> No Layout
-const publicRoutes = [
-    { path: "/login", component: Login, layout: null },
-    { path: "/register", component: Register, layout: null },
-  ];
-  
-const privateRoutes = [
-    { path: "/", component: Dashboard, layout: DefaultLayout },
-    { path: "/dashboard", component: Dashboard, layout: DefaultLayout },
-    { path: "/profile", component: Profile, layout: DefaultLayout },
-    { path: "/history", component: History, layout: DefaultLayout },
-    { path: "/rooms", component: Rooms, layout: DefaultLayout },
-    { path: "/rooms/:id/devices", component: Devices, layout: DefaultLayout },
-];
+// routes
+import MainRoutes from './MainRoutes';
+import AuthenticationRoutes from './AuthenticationRoutes';
 
-export { publicRoutes, privateRoutes };
+// ==============================|| ROUTING RENDER ||============================== //
+
+export default function ThemeRoutes() {
+  return useRoutes([MainRoutes, AuthenticationRoutes]);
+}
